2d91d0a5f67c1e793ba5c1f9995a62caf5c4249a
임운택 <wkblack11@gmail.com>
2018-05-14 04:40:41 +0900

    for a slack test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import server from './src/server'
 
 server.listen(3000, () => {
-  console.log('express server is running: ', 'http://localhost:3000')
+  console.log('express server is running: ', 'http://localhost:3000/')
 })
