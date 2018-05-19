344c353d869c216e61c7ef3d6d36fc15dae25dd4
anianddo <jun0151@naver.com>
2018-05-19 19:28:55 +0900

    gameroom_change_variable_name

--- src/socket/game.js
+++ src/socket/game.js
@@ -8,13 +8,13 @@ import { connect } from 'net'
  */
 const eventHandler = (io, socket) => {
   socket.on(M.ENTER_CHAT, async data => {
-    const userID = socket.handshake.session.user.id
-    console.log(user.id + user.nickname + user.money)
+    const userID = socket.handshake.session.player.id
+    console.log(player.id + player.nickname + player.money)
   })
   socket.on(M.CHAT_MSG, async message => {
-    const user = socket.handshake.session.user
+    const player = socket.handshake.session.player
     const roomID = socket.handshake.session.roomID
-    if (roomID) io.to(roomID).emit(M.CHAT_MSG, { name: user.name, message })
+    if (roomID) io.to(roomID).emit(M.CHAT_MSG, { name: player.name, message })
   })
   socket.on(M.EXIT_CHAT, async data => {
     console.log('EXIT CHAT')
